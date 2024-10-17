5bfccfb1d30e50eb11d64815504d81b5fde10c0f
Thaisgabrielae <thaisgabrielae@hotmail.com>
2024-10-16 21:51:14 -0300

    Add files via upload

--- JS/exercicios.js
+++ JS/exercicios.js
@@ -12,3 +12,63 @@ for(let i=1; i<=3; i++) {
         console.log(`O numero ${numero} é ímpar.`);
     }
 }
+
+
+//tabuada
+const rl = require('readline-sync');
+const tabuada = rl.questionInt("Qual a tabuada desejada? ");
+for(let multiplicador = 1; multiplicador <=10; multiplicador++) {
+
+console.log(`${tabuada} x ${multiplicador} = ${tabuada * multiplicador++}`);
+
+}
+
+
+
+// pedir 4 notas e fazer a media
+
+const rl = require('readline-sync');
+
+let soma = 0;
+
+for(let i = 1; i <= 4; i++) {
+    let nota = rl.questionFloat(`Informe a nota ${i} do aluno: `);
+    soma = soma + nota;
+}
+const media = soma / 4;
+console.log(media.toFixed(2));
+
+
+
+
+const readline = require('readline-sync');
+
+let soma = 0;
+
+while(soma < 100) {
+    let numero = readline.questionInt("Informe um numero para adicionar a soma: ");
+    soma = soma + numero;
+    console.log("soma: ", soma);
+}
+
+
+WHILE --- Enquanto
+let i = 0;
+while(i < 5) {
+    console.log(i);
+    i++;
+}
+
+const readline = require('readline-sync');
+
+let valor = 0;
+while(valor % 5 == 0) {
+    valor = readline.questionInt("Informe um numero multiplo de 5: ");
+    if (valor % 5 == 0) {
+        console.log('Numero: ', valor);
+        console.log('É múltiplo de 5')
+    } else {
+        console.log('Numero: ', valor);
+        console.log('Não é múltiplo de 5')
+    }
+}
--- JS/index.js
+++ JS/index.js
@@ -1,23 +1,29 @@
 // Laços de Repetição
+
+// SEI QUANTAS VEZES QUERO EXECUTAR?
+// SIM - UTILIZAR FOR
+// NÃO - UTILIZAR WHILE
+
+
 // FOR --- Para
 for(let i = 0; i < 5; i++) {
     console.log(i);
 }
 
 // WHILE --- Enquanto
+let i = 0;
+while(i < 5) {
+    console.log(i);
+    i++;
+}
 // DO WHILE -- Faça enquanto
 
-
-
-
-// Solicite um número para o usuário e faça a tabuada do 1 até o 10 daquele número
-const rl = require('readline-sync');
-
-const tabuada = rl.questionInt("Qual a tabuada desejada? ");
-
-for(let multiplicador = 1; multiplicador <= 10; multiplicador++) {
-    console.log(`${tabuada} x ${multiplicador} = ${tabuada*multiplicador}`);
-}
+let i = 10;
+do {
+    console.log(i);
+    i++;
+} while(i < 5);
+console.log('Fim do While');
 
 
 
@@ -27,25 +33,3 @@ for(let multiplicador = 1; multiplicador <= 10; multiplicador++) {
 
 
 
-
-//tabuada
-const rl = require('readline-sync');
-const tabuada = rl.questionInt("Qual a tabuada desejada? ");
-for(let multiplicador = 1; multiplicador <=10; multiplicador++) {
-
-console.log(`${tabuada} x ${multiplicador} = ${tabuada * multiplicador++}`);
-
-}
-
-// pedir 4 notas e fazer a media
-
-const rl = require('readline-sync');
-
-let soma = 0;
-
-for(let i = 1; i <= 4; i++) {
-    let nota = rl.questionFloat(`Informe a nota ${i} do aluno: `);
-    soma = soma + nota;
-}
-const media = soma / 4;
-console.log(media.toFixed(2));
